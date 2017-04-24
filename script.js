87a577c3f95380867dcbf4e6d2720341a96c67a0
lysander <oforilysander123@gmail.com>
2017-04-24 13:36:03 +0000

    Lysander dice

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
   function rolldice(){
-  //Place your code here 
-
-
-  };
+   var dice*7 = math.floor(math.random()+1
+  } 
   
-});
+  var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+ctx.rect(20,20,150,100);
+ctx.stroke();
+</script>
+
+    
+  //Place your code here 
+ //variabl
